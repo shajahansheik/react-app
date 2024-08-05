66b41ac03d0534d8bc341fdd7f5023b5c5df6c9c
Shajahan <shajahan@opstronomy.com>
2024-08-05 13:02:56 +0530

    added content reader

--- src/components/createpost.tsx
+++ src/components/createpost.tsx
@@ -3,10 +3,20 @@ import React, { useState } from 'react';
 
 function Createpost(props: any) {
     const [fileName, setFileName] = useState('No file chosen');
+    const [fileContent, setFileContent] = useState('');
     const handleFileChange = (event: any) => {
         const file = event.target.files[0];
-        
         setFileName(file ? file.name : 'No file chosen');
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e:any) => {
+                setFileContent(e.target.result);
+            };
+            reader.readAsText(file); // Assuming you want to read the file as text
+        } else {
+            setFileContent('');
+        }
     };
     return (
         <div className='space-y-2'>
@@ -19,21 +29,31 @@ function Createpost(props: any) {
                     <textarea placeholder='Description..' name="description" id="description" cols={30} rows={10} className='border hover:border-black px-2 py-2 active:outline-none w-1/3'></textarea>
                 </div>
                 <div className='flex flex-col items-center justify-center'>
-                     <div className='w-1/3'>
-                    <div className="flex items-center  gap-3">
-                        <input type="file" id="file" className="hidden" onChange={handleFileChange} />
-                        <label htmlFor="file" className="px-4 py-2 bg-blue-200 hover:bg-blue-400 text-white cursor-pointer rounded-lg text-lg border-none">Choose File</label>
-                        <span className="file-name" >{fileName}</span>
-                    </div>
+                    <div className='w-1/3'>
+                        <div className="flex items-center  gap-3">
+                            <input type="file" id="file" className="hidden" onChange={handleFileChange} />
+                            <label htmlFor="file" className="px-4 py-2 bg-blue-200 hover:bg-blue-400 text-white cursor-pointer rounded-lg text-lg border-none">Choose File</label>
+                            <span className="file-name" >{fileName}</span>
+                        </div>
                     </div>
                 </div>
+
+            <div>
                 
+                    <div className="file-content">
+                        <h3>File Content:</h3>
+                        
+                        <iframe src={fileContent} ></iframe>
+                    </div>
+                
+            </div>
                 <div className='flex flex-col items-center justify-center'>
                     <div className=' w-1/3 flex items-center justify-end'>
                         <button className='px-4 py-2 bg-green-300 hover:bg-green-500 text-white rounded-md'>Post</button>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 }
