7337c350263c2eca9db56a8967036dc22d88fa1d
Shajahan <shajahan@opstronomy.com>
2024-08-05 12:09:35 +0530

    tailwind added

--- src/components/createpost.tsx
+++ src/components/createpost.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Createpost(props: any) {
     return (
         <div>
-            <div>Create Post</div>
+            <div className='bg-red-100'>Create Post</div>
             <div>
                 <div>
                     <input type="text" placeholder="Title" />
